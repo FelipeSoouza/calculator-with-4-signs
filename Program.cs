0fd342a51f67bb3ba5e5caffff54b38ce768b82b
FelipeSoouza <146121300+FelipeSoouza@users.noreply.github.com>
2024-04-15 11:40:29 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CALCULADORA");
+            Console.WriteLine("-------------------------------------");
+
+            Console.WriteLine("Escolha qual o sinal que deseja fazer a operação:");
+            Console.WriteLine("Adição(+)");
+            Console.WriteLine("Subtração(-)");
+            Console.WriteLine("Multiplicação(*)");
+            Console.WriteLine("Divisão(/)");
+            string opera = Console.ReadLine();
+
+            if (opera == "+")
+            {
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o primeiro numero:");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o segundo numero:");
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o terceiro numero:");
+                double num3 = double.Parse(Console.ReadLine());
+
+                var result = num1 + num2 + num3;
+
+                Console.WriteLine("--------------------------------------");
+
+                Console.WriteLine(result);
+
+            }
+
+            else if (opera == "-")
+            {
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o primeiro numero:");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o segundo numero:");
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o terceiro numero:");
+                double num3 = double.Parse(Console.ReadLine());
+
+                var result = num1 - num2 - num3;
+
+                Console.WriteLine("--------------------------------------");
+
+                Console.WriteLine(result);
+
+            }
+
+            else if (opera == "*")
+            {
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o primeiro numero:");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o segundo numero:");
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o terceiro numero:");
+                double num3 = double.Parse(Console.ReadLine());
+
+                var result = num1 * num2 * num3;
+
+                Console.WriteLine("--------------------------------------");
+
+                Console.WriteLine(result);
+
+            }
+
+            else if (opera == "/")
+            {
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o primeiro numero:");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o segundo numero:");
+                double num2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine("Insira o terceiro numero:");
+                double num3 = double.Parse(Console.ReadLine());
+
+                var result = num1 / num2 / num3;
+
+                Console.WriteLine("--------------------------------------");
+
+                Console.WriteLine(result);
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
